33f6a8bb41ab693e1c75135560998c902f7bd322
Anthony <antho.tedde@gmail.com>
2017-10-23 16:17:52 +0200

    Compute expectation of random walk

--- R/rwexp.R
+++ R/rwexp.R
@@ -1,3 +1,9 @@
+#' Expectation of random walk.
+#' The expectation of the random walk as described in "stochastic calculus
+#' for finance" equal 0.
+#' Therefore the calculation made by this function is only a verification
+#' of the Expected zero growth for random walk.
+#' More details and calculation will come later.
 rwexp <- function(Mk){
-
+  sapply(Mk, function(x){sum(x[, 1] * x[, 2])})
 }
