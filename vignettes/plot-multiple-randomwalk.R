5d86358bdf0c0bb208b4272a7b890f84a68a33f5
Anthony <antho.tedde@gmail.com>
2017-10-31 11:15:58 +0100

    add more option to output vignettes

--- vignettes/plot-multiple-randomwalk.R
+++ vignettes/plot-multiple-randomwalk.R
@@ -1,3 +1,16 @@
 ## ------------------------------------------------------------------------
 rw <- srwalkGenerator(n = 100)
 
+## ------------------------------------------------------------------------
+summary(rw[[1]])
+
+## ------------------------------------------------------------------------
+ggplot2::ggplot(rw[[1]], ggplot2::aes(x = time_periods, y = random_walk_path)) + 
+  ggplot2::geom_line()
+
+## ------------------------------------------------------------------------
+ggplot2::ggplot(dplyr::bind_rows(rw, .id = "uniqueID"), 
+                ggplot2::aes(x = time_periods, y = random_walk_path, colour=uniqueID)) + 
+  ggplot2::geom_line() + 
+  ggplot2::theme(legend.position = 'none')
+
