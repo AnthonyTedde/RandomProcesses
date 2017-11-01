c446ddb89cb92ebea21f15632584bc2d7c7971dd
Anthony <antho.tedde@gmail.com>
2017-11-01 09:37:48 +0100

    add failing test for theoretical random walk

--- tests/testthat/testtrwalkGenerator.R
+++ tests/testthat/testtrwalkGenerator.R
@@ -3,13 +3,15 @@ library(RandomWalk)
 
 # Define preamble variables ----------------------------
 t <- 10
+sc <- 4
 
 # Define random walk object ----------------------------
 rw <- RandomWalk::trwalkGenerator(time_to_maturity = t)
 rw10 <- rw[[length(rw)]]
 
 scaledrw <- RandomWalk::trwalkGenerator(time_to_maturity = t,
-                                        scale = 4)
+                                        scale = sc)
+scaledrw1 <- scaledrw[[1 + 1]][, 'Mt']
 
 # Define expectation and STD var.----------------------------
  expectedX <- sum(rw10[, 'Mt'] * rw10[, 'Pr'])
@@ -24,3 +26,10 @@ test_that("Variance value is equal to time_to_maturity", {
   expect_equal(variance, t)
 })
 
+test_that("scaled RW increment against good multiplier", {
+  expect_equal(scaledrw1, c(1/sqrt(sc), (-1) * 1/sqrt(sc)))
+})
+
+test_that("# transaction period according to scale", {
+  expect_equal(length(scaledrw), (sc * t) + 1)
+})
