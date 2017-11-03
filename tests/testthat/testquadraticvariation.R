c911910aa4d41539f03cee5d0300ffb1709df4d7
Anthony <antho.tedde@gmail.com>
2017-11-03 09:41:30 +0100

    test: scaled rw accumulated quadratic var. at rate 1 per unit of time

--- tests/testthat/testquadraticvariation.R
+++ tests/testthat/testquadraticvariation.R
@@ -3,12 +3,21 @@ library(RandomWalk)
 #source("randomwalk.R", chdir = T)
 
 # Objects and variables declaration ----------------------------
-timeT <- 10
+timeT <- 4
+divider <- 100
 rw <- srwalk(time_to_maturity = timeT)
 Q <- quadraticVariation(rw, timeT)
 
+srw <- srwalk(time_to_maturity = timeT,
+              scale = divider)
+Qsrw <- quadraticVariation(srw, timeT)
+
 # TEST CASE ----------------------------
 
+# The following test case is performed for a symmetric random walk
+# and for a scaled radom walk.
 test_that("Quadratic variation grow at rate 1 / unit of time dt", {
   expect_equal(Q, timeT)
+  expect_equal(Qsrw, timeT)
 })
+
