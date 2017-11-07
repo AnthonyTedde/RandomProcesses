32abdcc1d087c6d64a56e29744ac2a0dd849fb64
Anthony <antho.tedde@gmail.com>
2017-11-07 10:30:00 +0100

    add some tests for the brownian motion object constructor

---
+++ R/sbmotion.R
@@ -0,0 +1,6 @@
+#' Brownian motion generator
+#' @param seed - Define the seed sequence to alway get the same sampled
+#' randomly generated brownian motion.
+sbmotion <- function(){
+
+}
--- tests/testthat/testsbmotion.R
+++ tests/testthat/testsbmotion.R
@@ -2,3 +2,21 @@ library(testthat)
 library(RandomWalk)
 
 # will be tested some cases on brownian motion and its properties.
+timeT <- 4
+p <- c('head' = 1/2, 'tail' = 1/2)
+s <- 100
+seed <- 1
+
+bm <- sbmotion(time_to_maturity = timeT,
+               prob = p,
+               seed = seed,
+               scale = s)
+test_that("Brownian motion has belongs to correct class", {
+  expect_equal(class(bm), c('data.frame', 'brownianmotion'))
+})
+
+test_that("Brownian motion has correct size", {
+  expect_equal(nrow(bm), timeT * s + 1)
+})
+
+
