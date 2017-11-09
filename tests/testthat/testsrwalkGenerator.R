5a5431d2d2a2fde1fcf339398bfd7c95c4dc180c
Anthony <antho.tedde@gmail.com>
2017-11-09 11:13:21 +0100

    write the first test cases for the sample randomwalk generator

--- tests/testthat/testsrwalkGenerator.R
+++ tests/testthat/testsrwalkGenerator.R
@@ -10,6 +10,18 @@ rw <- srwalkGenerator(time_to_maturity = timeT,
                       seed = 4,
                       n = n)
 
-test_that("The generator returns the right number of sampled random walk")
-test_that("The generator gives reproducible sampled random walks")
-test_that("The generator provides the good classes for its generated sampled random walk")
+reproducible_rw <- srwalkGenerator(time_to_maturity = timeT,
+                                   scale = divider,
+                                   seed = 4,
+                                   n = n)
+
+test_that("The generator returns the right number of sampled random walk", {
+  expect_equal(length(rw), n)
+})
+test_that("The generator gives reproducible sampled random walks", {
+  expect_equal(rw, reproducible_rw)
+})
+test_that("The generator provides the good classes for its generated sampled random walk", {
+  expect_equal(class(rw), c('sampled_randomwalk', 'list'))
+  sapply(rw, FUN = function(rwalk) expect_equal(class(rwalk), c('sampled_randomwalk', 'data.frame')))
+})
