f2b62a0ebacc05b5992d242e4f3b41f53c8b9e91
Anthony <antho.tedde@gmail.com>
2017-11-19 10:28:26 +0100

    Add test to ensure to provide reproducible Bronian Motion.

--- tests/testthat/test_sbmotion.R
+++ tests/testthat/test_sbmotion.R
@@ -28,3 +28,9 @@ test_that("Brownian motion has correct size", {
 test_that("With same seed, sampled generated brownian motion must be reproducible", {
   expect_equal(sbmotion(seed = 4), sbmotion(seed = 4))
 })
+
+test_that("With same seed but different scale, Brownian Motion should be reproducible", {
+  bm1 <- sbmotion(time_to_maturity = 4, scale = 100)
+  bm2 <- sbmotion(time_to_maturity = 4, scale = 200)
+  expect_equal(bm1, bm2[seq(1, nrow(bm2), by = 2), ])
+})
